622bea369b61a0438eb808d97abc6f4a35ac3fff
huanghailong <huanghailong17@mails.ucas.ac.cn>
2019-01-23 22:25:25 +0800

    rm useless bash shell & rm <unk> in predict names

--- app_name_generator/predict.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-version=$1
-python -u predict.py --root_path=model_$version --batch_size=1 --use_beam_search=True
--- app_name_generator/train.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-version=$1
-python -u train.py --root_path=model_$version
