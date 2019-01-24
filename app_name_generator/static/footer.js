50178711df96df95ebab3a55dc32bffa0bd5934e
andrewcao95 <andrewcao95@163.com>
2019-01-24 09:57:29 +0800

    update page style

--- app_name_generator/static/footer.js
+++ app_name_generator/static/footer.js
@@ -4,7 +4,7 @@ window.onload = function() {
     // Find the 'myfooter' element.
     var elem = document.getElementById('myfooter');
     if (!!elem) {
-        elem.innerHTML = '<p><b>Team Members</b>: <i>Hailong Huang(Institute of Software, CAS), Lu Cao(Peking University), Jiansong Li(Institute of Computing Technology, CAS)</i></p>';
+        elem.innerHTML = '<p>© 2019 <i>Hailong Huang(Institute of Software, CAS), Lu Cao(Peking University), Jiansong Li(Institute of Computing Technology, CAS)</i>. All Rights Reserved.</p>';
     }
     else {
         alert('WARN! Element "myfooter" not found!');
