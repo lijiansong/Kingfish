6a5b4b1d46b961de78b056a477d8ad2a88ea3ead
andrewcao95 <andrewcao95@163.com>
2019-01-24 10:29:52 +0800

    update

--- app_name_generator/static/footer.js
+++ app_name_generator/static/footer.js
@@ -4,7 +4,7 @@ window.onload = function() {
     // Find the 'myfooter' element.
     var elem = document.getElementById('myfooter');
     if (!!elem) {
-        elem.innerHTML = '<p>© 2019 <i>Hailong Huang(Institute of Software, CAS), Lu Cao(Peking University), Jiansong Li(Institute of Computing Technology, CAS)</i>. All Rights Reserved.</p>';
+        elem.innerHTML = '<p>© 2019 <i>Hailong Huang (Institute of Software, CAS), Lu Cao (Peking University), Jiansong Li (Institute of Computing Technology, CAS)</i>. All Rights Reserved.</p>';
     }
     else {
         alert('WARN! Element "myfooter" not found!');
